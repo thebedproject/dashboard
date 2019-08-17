639b83eeb3e9885e219684199bf38797b2fed32f
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-08-17 19:16:53 +0100

    adds position to maps

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,18 +3,20 @@
 </template>
 
 <script>
+import * as axios from "axios";
 import gmapsInit from "../utils/gmaps";
 
 export default {
   name: "Map",
-  props: {},
   async mounted() {
     try {
       const google = await gmapsInit();
       const geocoder = new google.maps.Geocoder();
-      const map = new google.maps.Map(this.$el);
+      const map = new google.maps.Map(this.$el, {
+        disableDefaultUI: true
+      });
 
-      geocoder.geocode({ address: "Austria" }, (results, status) => {
+      geocoder.geocode({ address: "London" }, (results, status) => {
         if (status !== "OK" || !results[0]) {
           throw new Error(status);
         }
@@ -22,9 +24,27 @@ export default {
         map.setCenter(results[0].geometry.location);
         map.fitBounds(results[0].geometry.viewport);
       });
+
+      const locations = await this.getDonations();
+      locations.map(location => {
+        const position = {
+          lat: location.lat,
+          lng: location.lon
+        };
+        const title = location.amount.toString();
+        new google.maps.Marker({ position, map, title });
+      });
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log(error);
+    }
+  },
+  methods: {
+    getDonations: async function() {
+      const result = await axios.get(
+        "https://hdpkjgu3s9.execute-api.eu-west-2.amazonaws.com/test/donations"
+      );
+      return result.data.donationsResult;
     }
   }
 };
@@ -33,6 +53,6 @@ export default {
 <style>
 .Map {
   width: 100vw;
-  height: 100vh;
+  height: 96vh;
 }
 </style>
